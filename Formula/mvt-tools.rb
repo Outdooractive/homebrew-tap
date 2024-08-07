7c3a4e8e72a72f40b4a8e3c86c1d48183e84c0aa
Thomas Rasch <thomas.rasch@outdooractive.com>
2024-08-07 10:15:27 +0200

    Wrong class name...

--- Formula/mvt-tools.rb
+++ Formula/mvt-tools.rb
@@ -1,4 +1,4 @@
-class MVTTools < Formula
+class MvtTools < Formula
   desc "A command line tool for working with Mapbox Vector Tiles (MVT)"
   homepage ""
   url "https://github.com/Outdooractive/mvt-tools/releases/download/v1.7.0/mvt.tar.gz"
