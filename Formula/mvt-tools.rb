16234280808aed51a0042b9651254d6549fd2ac4
Thomas Rasch <trasch@users.noreply.github.com>
2024-08-27 14:54:17 +0200

    mvt-tools 1.8.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/mvt-tools.rb
+++ Formula/mvt-tools.rb
@@ -1,10 +1,10 @@
 class MvtTools < Formula
   desc "A command line tool for working with Mapbox Vector Tiles (MVT)"
   homepage ""
-  url "https://github.com/Outdooractive/mvt-tools/releases/download/v1.8.0/mvt.tar.gz"
-  sha256 "e03caaf3707fd2e6e85944525ea0038d414d4f39fa166f3727de0ab2f8088cdd"
+  url "https://github.com/Outdooractive/mvt-tools/releases/download/v1.8.1/mvt.tar.gz"
+  sha256 "bd54d5adca8cc812297f4370439f49c2f86782c5117b8b89bdf70bc731e5e43a"
   license "MIT"
-  version "1.8.0"
+  version "1.8.1"
   
   def install
     bin.install "mvt"
