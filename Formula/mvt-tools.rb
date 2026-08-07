aebaaf115f7b2451254979841170b250d8fff907
Thomas Rasch <thomas.rasch@outdooractive.com>
2026-08-07 14:02:48 +0200

    mvt-tools 2.4.0

--- Formula/mvt-tools.rb
+++ Formula/mvt-tools.rb
@@ -4,7 +4,7 @@ class MvtTools < Formula
   url "https://github.com/Outdooractive/mvt-tools/releases/download/v2.2.3/mvt.tar.gz"
   sha256 "39315dc1bea639b18969d256d47a6e6602a5f663905d4af3bf4b9fc20f3ab678"
   license "MIT"
-  version "2.2.3"
+  version "2.4.0"
   
   def install
     bin.install "mvt"
