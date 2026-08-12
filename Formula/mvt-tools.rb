b642c6c3ed153e77b9d686829f084bee49e91365
Thomas Rasch <thomas.rasch@outdooractive.com>
2026-08-12 17:11:07 +0200

    Got it messed up...

--- Formula/mvt-tools.rb
+++ Formula/mvt-tools.rb
@@ -1,8 +1,8 @@
 class MvtTools < Formula
   desc "A command line tool for working with geo data (GeoJSON, MLT, MVT, GeoPackage, Shapefile, FIT, CSV)"
   homepage ""
-  url "https://github.com/Outdooractive/mvt-tools/archive/refs/tags/v2.4.1.tar.gz"
-  sha256 "6d277173500e13d9e5abea59a7f8e0b0d14c60ad166b9905c9796e53a26307b7"
+  url "https://github.com/Outdooractive/mvt-tools/releases/download/v2.4.1/mvt.tar.gz"
+  sha256 "9b134a974a3768571e7ca18209fb6e5d2d69ba7f2660bd65d7be50d69fe98880"
   license "MIT"
   version "2.4.1"
   
