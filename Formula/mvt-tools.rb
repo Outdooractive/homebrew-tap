d148cf79c05ff58d6a8edb0a9bb1e7c7a4046d1a
Thomas Rasch <thomas.rasch@outdooractive.com>
2026-08-13 13:35:16 +0200

    mvt-tools 2.4.2

--- Formula/mvt-tools.rb
+++ Formula/mvt-tools.rb
@@ -1,10 +1,10 @@
 class MvtTools < Formula
   desc "A command line tool for working with geo data (GeoJSON, MLT, MVT, GeoPackage, Shapefile, FIT, CSV)"
   homepage ""
-  url "https://github.com/Outdooractive/mvt-tools/releases/download/v2.4.1/mvt.tar.gz"
-  sha256 "9b134a974a3768571e7ca18209fb6e5d2d69ba7f2660bd65d7be50d69fe98880"
+  url "https://github.com/Outdooractive/mvt-tools/releases/download/v2.4.2/mvt.tar.gz"
+  sha256 "b8c47356c2de8fb8d8c21beaa170044db09da1e11b88c552fac328ce5b93a7c7"
   license "MIT"
-  version "2.4.1"
+  version "2.4.2"
   
   def install
     bin.install "mvt"
